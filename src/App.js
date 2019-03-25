06cfd9ac97e95e2c0e431f1b2010cb9fc0b1c3bb
erica-y-chen <eyufanchen@gmail.com>
2019-03-25 15:22:43 -0700

    completed header and post, need to do comment

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar/searchBar';
+import PostContainer from './components/PostContainer/postContainer'
+import DummyData from './dummy-data'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      dummydata: DummyData,
+      thumbnailUrl: '',
+      username: '', 
+      likes: '', 
+      timestamp: '',
+      imageUrl: '',
+      comments: [],
+      likes: {},
+    };
+    // console.log(dummydata);
+  }
+
+
+  render() {  
+    return (
+      <div className="App"> 
+        <SearchBar />
+        <PostContainer  
+        post= {this.state.dummydata}
+        thumbnailUrl = {this.state.thumbnailUrl}
+        username = {this.state.username}
+        imageUrl = {this.state.imageUrl}
+        comments = {this.state.comments}
+        likes = {this.state.likes}
+        />
+      </div> 
+    );
+  }
+} 
+
+export default App;
